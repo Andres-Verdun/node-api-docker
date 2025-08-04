f050061099113a0c73d96d7af633a05e3b70d57f
Andrés <andresverdun1991@gmail.com>
2025-08-04 20:15:00 -0300

    feat: cloné y levanté la API en Docker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send({
-    message: "Here's your response from the node.js 🐳 container",
+    message: "Here's your response from the node.js 🐳 container. Bienvenido",
   });
 });
 
